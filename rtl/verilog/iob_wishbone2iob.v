8c940684a78d66d53bedb9cd6e73622b93a61731
Pedro Antunes <pedronmantunes@gmail.com>
2023-04-28 23:03:14 +0200

    Simulation: passes all tests;

--- rtl/verilog/iob_wishbone2iob.v
+++ rtl/verilog/iob_wishbone2iob.v
@@ -29,7 +29,8 @@ module iob_wishbone2iob #(
     
     // IOb auxiliar wires
     wire valid_r;
-    wire valid_e;
+    wire valid;
+    wire ready_r;
     wire [DATA_W/8-1:0] wstrb;
     wire [DATA_W/8-1:0] wstrb_r;
     wire [DATA_W-1:0] rdata_r;
@@ -38,22 +39,23 @@ module iob_wishbone2iob #(
     wire [DATA_W-1:0] wb_data_r;
 
     // Logic
-    assign valid_o = (wb_stb_i)&(~valid_r);
-    assign address_o  = valid_o? wb_addr_i:wb_addr_r;
-    assign wdata_o = valid_o? wb_data_i:wb_data_r;
-    assign wstrb_o = valid_o? wstrb:wstrb_r;
+    assign valid_o = (valid)&(~valid_r);
+    assign address_o  = valid? wb_addr_i:wb_addr_r;
+    assign wdata_o = valid? wb_data_i:wb_data_r;
+    assign wstrb_o = valid? wstrb:wstrb_r;
     
+    assign valid = wb_stb_i&wb_cyc_i;
+    assign wstrb = wb_we_i? wb_select_i:4'h0;
+
+    iob_reg #(1,0) iob_reg_valid (clk_i, arst_i, ready_i, valid, 1'b1, valid_r);
+    iob_reg #(ADDR_W,0) iob_reg_addr (clk_i, arst_i, 1'b0, valid, wb_addr_i, wb_addr_r);
+    iob_reg #(DATA_W,0) iob_reg_data_i (clk_i, arst_i, 1'b0, valid, wb_data_i, wb_data_r);
+    iob_reg #(DATA_W/8,0) iob_reg_strb (clk_i, arst_i, 1'b0, valid, wstrb, wstrb_r);
+
     assign wb_data_o = ready_i? rdata_i:rdata_r;
     assign wb_ack_o = ready_i;
     assign wb_error_o = 1'b0;
-
-    assign wstrb = wb_we_i? wb_select_i:4'h0;
-
-    assign valid_e = valid_o|ready_i;
-    iob_reg #(1,0) iob_reg_valid (clk_i, arst_i, 1'b0, valid_e, valid_o, valid_r);
-    iob_reg #(ADDR_W,0) iob_reg_addr (clk_i, arst_i, 1'b0, valid_o, wb_addr_i, wb_addr_r);
-    iob_reg #(DATA_W,0) iob_reg_data_i (clk_i, arst_i, 1'b0, valid_o, wb_data_i, wb_data_r);
-    iob_reg #(DATA_W/8,0) iob_reg_strb (clk_i, arst_i, 1'b0, valid_o, wstrb, wstrb_r);
+    iob_reg #(1,0) iob_reg_ready (clk_i, arst_i, ~valid, ready_i, 1'b1, ready_r);
     iob_reg #(DATA_W,0) iob_reg_data_o (clk_i, arst_i, 1'b0, ready_i, rdata_i, rdata_r);
 
 
